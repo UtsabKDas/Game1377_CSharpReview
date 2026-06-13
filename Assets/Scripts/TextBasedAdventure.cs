9041b88c53ae29ea4d601c222cb186510a218014
UtsabKDas <37959749+UtsabKDas@users.noreply.github.com>
2026-06-12 23:44:17 -0500

    Added Player navigation through an area
    Added tileNames string array to represent locations.
    Added ability to move through the array using D and A keys to change the index.
    Added return statement to early out when there is no input
    Added check for valid index.

--- Assets/Scripts/TextBasedAdventure.cs
+++ Assets/Scripts/TextBasedAdventure.cs
@@ -2,17 +2,42 @@ using UnityEngine;
 
 public class TextBasedAdventure : MonoBehaviour
 {
+    string[] tileNames = { "Dark Cave", "Mossy Tunnel", "Crystal Room" };
 
-
+    int playerIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("You are in: " + tileNames[playerIndex]);
     }
 
     // Update is called once per frame
     void Update()
     {
+        int newIndex = playerIndex;
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            newIndex++;
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            newIndex--;
+        }
+        else
+        {
+            return;
+        }
+
+        if (newIndex >= 0 && newIndex < tileNames.Length)
+        {
+            playerIndex = newIndex;
+        }
+        else
+        {
+            Debug.Log("Can't go that way");
+        }
+        Debug.Log("You are in: " + tileNames[newIndex]);
+
         
     }
 }
