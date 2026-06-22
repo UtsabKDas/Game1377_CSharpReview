5e2f841801c20a1dcaacd28a2452de815d2f1038
UtsabKDas <37959749+UtsabKDas@users.noreply.github.com>
2026-06-21 22:21:15 -0500

    Update TextBasedAdventure.cs

--- Assets/Scripts/TextBasedAdventure.cs
+++ Assets/Scripts/TextBasedAdventure.cs
@@ -8,12 +8,7 @@ public class TextBasedAdventure : MonoBehaviour
     {
         public string Name;
         public TileType Type;
-    }
-
-    [System.Serializable]
-    public struct RoomRow
-    {
-        public Room[] rooms;
+        public Enemy RoomEnemy;
     }
 
     public enum TileType
@@ -21,50 +16,74 @@ public class TextBasedAdventure : MonoBehaviour
         Invalid,
         Empty,
         Item,
-        Enemy,
+        EnemyEncounter,
         Exit,
     }
 
+    public enum DamageType
+    {
+        Invalid,
+        Normal,
+        Poison,
+        Curse,
+    }
+
+    public struct StatusEffect
+    {
+        public bool IsActive;
+        public DamageType Type;
+        public int TurnsRemaining;
+        public int DamagePerTurn;
+    }
+
+    public struct Enemy
+    {
+        public string Name;
+        public int Damage;
+        public DamageType DamageType;
+    }
+
     private Room[,] dungeon = { 
-                                {   new Room { Name = "Dark Cave",    Type = TileType.Empty},
-                                    new Room { Name = "Mossy Tunnel", Type = TileType.Item},
-                                    new Room { Name = "Crystal Room", Type = TileType.Empty} },
+                                {   new Room { Name = "Dark Cave",    Type = TileType.Empty } ,
+                                    new Room { Name = "Mossy Tunnel", Type = TileType.Item  },
+                                    new Room { Name = "Crystal Room", Type = TileType.Empty } 
+                                },
                                 
-                                {   new Room { Name = "Bone Chamber", Type = TileType.Enemy },
-                                    new Room { Name = "Flooded Hall", Type = TileType.Empty},
-                                    new Room { Name = "Iron Gate",    Type = TileType.Exit } }
+                                {   new Room { Name = "Bone Chamber", Type = TileType.EnemyEncounter, RoomEnemy = new Enemy { Name = "Goblin", Damage = 1, DamageType = DamageType.Normal } },
+                                    new Room { Name = "Flooded Hall", Type = TileType.Empty },
+                                    new Room { Name = "Iron Gate",    Type = TileType.Exit  } 
+                                },
+
+                                {   new Room { Name = "Goblin Den",   Type = TileType.Empty },
+                                    new Room { Name = "Armory",       Type = TileType.EnemyEncounter, RoomEnemy = new Enemy { Name = "Orc", Damage = 2, DamageType = DamageType.Normal } },
+                                    new Room { Name = "Throne Room",  Type = TileType.Item  } 
+                                }
                               };
-    
-
-    private string[,] tileNames = { { "Dark Cave"   /* 0,0 */,  "Mossy Tunnel" /* 0,1 */,   "Crystal Room" /* 0,2 */ },
-                                    { "Bone Chamber"/* 1,0 */,  "Flooded Hall" /* 1,1 */,   "Iron Gate"              },
-                                    { "Goblin Den",             "Armory",                   "Throne Room"            }
-                                    };
-
-    private TileType[,] tileTypes = {   { TileType.Empty, TileType.Item,  TileType.Empty},
-                                        { TileType.Enemy, TileType.Empty, TileType.Exit },
-                                        { TileType.Empty, TileType.Enemy, TileType.Item }
-                                    };
 
     private int playerRow = 0;
     private int playerCol = 0;
-    private int playerHealth = 10;
-    private int enemyDamage = 1;
-    private int itemHealAmount = 2;
 
-    private Rigidbody rb;
+    public struct PlayerState
+    {
+        public int Health;
+        public StatusEffect CurrentStatusEffect;
+    }
+
+    private PlayerState currentPlayerState;
+
+    private int itemHealAmount = 2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentPlayerState.Health = 10;
         OutputTileInformation();
-        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool wasKeyPressed = HandleInput(out int newRow, out int newCol);
+        bool wasKeyPressed = HandleMovementInput(out int newRow, out int newCol);
         if (!wasKeyPressed)
         {
             return;
@@ -75,14 +94,14 @@ public class TextBasedAdventure : MonoBehaviour
 
     private void OutputTileInformation()
     {
-        Debug.Log("You are in: " + tileNames[playerRow, playerCol]);
+        Debug.Log("You are in: " + dungeon[playerRow, playerCol].Name);
 
-        switch (tileTypes[playerRow, playerCol])
+        switch (dungeon[playerRow, playerCol].Type)
         {
             case TileType.Empty:
                 Debug.Log("There is nothing here.");
                 break;
-            case TileType.Enemy:
+            case TileType.EnemyEncounter:
                 Debug.Log("Oooo a spooky ghost");
                 EncounterEnemy();
                 break;
@@ -101,7 +120,7 @@ public class TextBasedAdventure : MonoBehaviour
 
     private void EncounterEnemy()
     {
-        PlayerTakeDamage(enemyDamage);
+        PlayerTakeDamage(dungeon[playerRow, playerCol].RoomEnemy.Damage);
     }
     
     private void ItemPickup()
@@ -111,17 +130,17 @@ public class TextBasedAdventure : MonoBehaviour
 
     private void PlayerHeal(int heal)
     {
-        playerHealth += heal;
-        Debug.Log("You get healed. Your health is now " + playerHealth);
+        currentPlayerState.Health += heal;
+        Debug.Log("You get healed. Your health is now " + currentPlayerState.Health);
     }
 
     private void PlayerTakeDamage(int damage)
     {
-        playerHealth -= damage;
-        Debug.Log("You get hit. Your health is now " + playerHealth);
-        if (playerHealth <= 0)
+        currentPlayerState.Health -= damage;
+        Debug.Log("You get hit. Your health is now " + currentPlayerState.Health);
+        if (currentPlayerState.Health <= 0)
         {
-            playerHealth = 0;
+            currentPlayerState.Health = 0;
             Debug.Log("You are dead");
         }
     }
@@ -152,7 +171,7 @@ public class TextBasedAdventure : MonoBehaviour
     /// <returns>True if it is within the bounds, false if not</returns>
     private bool CheckIfNewPositionInTileBounds(int newRow, int newCol)
     {
-        return (newRow >= 0 && newRow < tileNames.GetLength(0)) && (newCol >= 0 && newCol < tileNames.GetLength(1));
+        return (newRow >= 0 && newRow < dungeon.GetLength(0)) && (newCol >= 0 && newCol < dungeon.GetLength(1));
     }
 
     /// <summary>
@@ -160,8 +179,8 @@ public class TextBasedAdventure : MonoBehaviour
     /// </summary>
     /// <param name="newRow">new row position</param>
     /// <param name="newCol">new column position</param>
-    /// <returns>True if an input was pressed, false if not</returns>
-    private bool HandleInput(out int newRow, out int newCol)
+    /// <returns>True if a movement input was pressed, false if not</returns>
+    private bool HandleMovementInput(out int newRow, out int newCol)
     {
         bool hasPressedKey = true;
         newRow = playerRow;
